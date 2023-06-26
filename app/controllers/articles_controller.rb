7948f47573fa42f10fd8d7262d75cd2c95b8cdc7
nao836 <naogm836@gmail.com>
2023-06-26 20:26:53 +0900

    install kaminari

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.page(params[:page]).per(10)
   end
 
   # GET /articles/1 or /articles/1.json
